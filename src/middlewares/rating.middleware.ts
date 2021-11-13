0d643cabaa5a21a1d460b5f47fc9fbce1c5d7df6
Michelle Romero <33272156+Marhdz@users.noreply.github.com>
2021-11-13 12:31:16 -0600

    Update rating.middleware.ts

--- src/middlewares/rating.middleware.ts
+++ src/middlewares/rating.middleware.ts
@@ -1,6 +1,6 @@
 export const validateSimulationRatingRequiredParamsMiddleware = async (req: any, res: any, next: any) => {
   if (!req.body.rating) {
-    return res.status(400).send({ success: false, message: `O campo "Avaliação" é obrigatório.` })
+    return res.status(400).send({ success: false, message: `El campo "Evaluación" es obligatorio.` })
   } else {
     return next();
   }

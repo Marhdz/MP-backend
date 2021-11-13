fddc8dc6d99a2e7e17c1010a1a924a2eb57bb66e
Michelle Romero <33272156+Marhdz@users.noreply.github.com>
2021-11-13 12:30:48 -0600

    Update simulation.middleware.ts

--- src/middlewares/simulation.middleware.ts
+++ src/middlewares/simulation.middleware.ts
@@ -2,25 +2,25 @@ import { algorithmNamesList } from "../utils/algorithmList"
 
 export const validateSimulationRequiredParamsMiddleware = async (req: any, res: any, next: any) => {
   if (!req.body.memorySize) {
-    return res.status(400).send({ success: false, message: `O campo "Tamanho da memória" é obrigatório.` })
+    return res.status(400).send({ success: false, message: `El campo "Tamaño de la memoria" es obligatorio.` })
   } else if (!req.body.pagesQueueSize) {
-    return res.status(400).send({ success: false, message: `O campo "Tamanho da fila de páginas" é obrigatório.` })
+    return res.status(400).send({ success: false, message: `El campo "Tamaño de cola de página" es obligatorio.` })
   } else if (!req.body.numberOfPages) {
-    return res.status(400).send({ success: false, message: `O campo "Quantidade de páginas" é obrigatório.` })
+    return res.status(400).send({ success: false, message: `El campo "Número de páginas" es obligatorio.` })
   } else if (!req.body.pages) {
-    return res.status(400).send({ success: false, message: `O campo "Páginas" é obrigatório.` })
+    return res.status(400).send({ success: false, message: `El campo "Páginas" es obligatorio.` })
   } else if (!req.body.pagesQueue) {
-    return res.status(400).send({ success: false, message: `O campo "Fila de páginas" é obrigatório.` })
+    return res.status(400).send({ success: false, message: `El campo "Cola de páginas" es obligatorio.` })
   } else if (!req.body.actionsQueue) {
-    return res.status(400).send({ success: false, message: `O campo "Fila de ações" é obrigatório.` })
+    return res.status(400).send({ success: false, message: `El campo "Cola de acciones" es obligatorio.` })
   } else if (!req.body.memoryInitalState) {
-    return res.status(400).send({ success: false, message: `O campo "Estado inicial da memória" é obrigatório.` })
+    return res.status(400).send({ success: false, message: `El campo "Estado inicial de la memoria" es obligatorio.` })
   } else if (!req.body.algorithms) {
-    return res.status(400).send({ success: false, message: `O campo "Algoritmos" é obrigatório.` })
+    return res.status(400).send({ success: false, message: `El campo "Algoritmos" es obligatorio.` })
   } else if (!req.body.clockInterruption && (req.body.algorithms.includes("nruAlgorithm") || req.body.algorithms.includes("secondChanceAlgorithm") || req.body.algorithms.includes("wsClockAlgorithm"))) {
-    return res.status(400).send({ success: false, message: `O campo "Interrupção do relógio" é obrigatório.` })
+    return res.status(400).send({ success: false, message: `El campo "Interrupción del reloj" es obligatorio.` })
   } else if (!req.body.tau && req.body.algorithms.includes("wsClockAlgorithm")) {
-    return res.status(400).send({ success: false, message: `O campo "τ (tau)" é obrigatório.` })
+    return res.status(400).send({ success: false, message: `El campo "τ (tau)" es obligatorio.` })
   } else {
     return next();
   }
@@ -41,39 +41,39 @@ export const validateSimulationParamsIntegrityMiddleware = async (req: any, res:
   if (memorySize !== memoryInitalState.length) {
     return res.status(400).send({
       success: false,
-      message: `O campo "Estado inicial da memória" deve possuir comprimento igual ao tamanho da memória.`
+      message: `El campo "Estado de memoria inicial" debe tener una longitud igual al tamaño de la memoria.`
     })
   } else if (pagesQueueSize !== pagesQueue.length) {
     return res.status(400).send({
       success: false,
-      message: `O campo "Fila de páginas" deve possuir comprimento igual ao tamanho da fila de páginas.`
+      message: `El campo "Cola de páginas" debe tener una longitud igual al tamaño de la cola de páginas.`
     })
   } else if (pagesQueueSize !== actionsQueue.length) {
     return res.status(400).send({
       success: false,
-      message: `O campo "Fila de ações" deve possuir comprimento igual ao tamanho da fila de páginas.`
+      message: `El campo "Cola de acciones" debe tener una longitud igual al tamaño de la cola de páginas.`
     })
   } else if (actionsQueueCharNotAllowed.length) {
     const plural = actionsQueueCharNotAllowed.length > 1 ? "s" : "";
     return res.status(400).send({
       success: false,
-      message: `O campo "Fila de ações" deve possuir apenas os caracteres "|", "E" e "L". Caracter${plural ? "es" : ""} não suportado${plural}: ${actionsQueueCharNotAllowed.join(", ")}.`
+      message: `El campo "Cola de acciones" debe tener solo los caracteres "|", "E" e "L". Caracter${plural ? "es" : ""} no soportados${plural}: ${actionsQueueCharNotAllowed.join(", ")}.`
     })
   } else if (numberOfPages !== pages.length) {
     return res.status(400).send({
       success: false,
-      message: `O campo "Páginas" deve possuir comprimento igual à quantidade de páginas.`
+      message: `El campo "Páginas" debe tener una longitud igual al número de páginas.`
     })
   } else if (clockInterruption >= pagesQueueSize) {
     return res.status(400).send({
       success: false,
-      message: `O campo "Interrupção do relógio" deve ser menor que o tamanho da fila de páginas.`
+      message: `El campo "Interrupción del reloj" debe ser más pequeño que el tamaño de la cola de páginas.`
     })
   } else if (algorithmsNotAllowed.length) {
     const plural = algorithmsNotAllowed.length > 1 ? "s" : "";
     return res.status(400).send({
       success: false,
-      message: `Algoritmo${plural} não suportado${plural}: ${algorithmsNotAllowed.join(", ")}.`
+      message: `Algoritmo${plural} no soportado${plural}: ${algorithmsNotAllowed.join(", ")}.`
     })
   } else {
     return next();
